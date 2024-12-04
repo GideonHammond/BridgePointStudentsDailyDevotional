924d4c368f7cd4321a3822469f6961392cbcf2e4
Gideon Hammond <gidoneson@gmail.com>
2024-12-04 10:16:57 -0700

    Update 1
    Header and stuff added... basic visuals!

--- myscript.js
+++ myscript.js
@@ -13,3 +13,14 @@ function checkHeadHeight(){
 window.onload = function() {
     checkHeadHeight();
 }
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
