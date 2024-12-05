995a65a98cac10b096675416e312a9a914e64432
Gideon Hammond <gidoneson@gmail.com>
2024-12-04 17:53:03 -0700

    Update 2
    Got the scrolly thing to work added the bible bar and started working on media queries for screens.

--- myscript.js
+++ myscript.js
@@ -14,13 +14,56 @@ window.onload = function() {
     checkHeadHeight();
 }
 
-const observer = new IntersectionObserver((entries) => {
+const intersectObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
         if (entry.isIntersecting){
             entry.target.classList.add("show");
         }
     });
 });
+
 const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements.forEach((el) => intersectObserver.observe(el));
+
+let isVisible = null;
+let scrollStart = false;
+let isShrunk = false;
+let isScrolling = false;
+const hero = $("hero-text");
+const container = $("main");
+setTimeout(enableScroll, 1500);
+const options = {
+    root:container,
+    threshold:1
+};
+const callBack = (entries) => {
+    isVisible = entries[0].isIntersecting;
+};
+const observer = new IntersectionObserver(callBack,options);
+observer.observe(hero);
+function enableScroll(){
+    scrollStart = true;
+}
+
+
+window.onscroll = function (e) {
+    if (isScrolling) {
+        return;
+    }
+
+    if(!scrollStart){
+        scrollTo(0,0);
+    }
+    if(isVisible && !isShrunk){
+        hero.classList.add("shrunk");
+        isShrunk = true;
+    }else if(isVisible){
+        hero.classList.remove("shrunk");
+        isShrunk = false;
+    }
+    isScrolling = true;
+    setTimeout(() => {
+        isScrolling = false;
+      }, 500);
+}
+
