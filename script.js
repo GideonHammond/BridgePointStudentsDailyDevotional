aa61b39abdb83a7f65c3cfa34ab590f5236a70ab
Gideon Hammond <gidoneson@gmail.com>
2025-04-21 11:55:27 -0600

    PR 1.00 test 3

--- script.js
+++ script.js
@@ -39,6 +39,6 @@ function closeNav() {
 fetch('devotionals.json')
             .then(response => response.json())
             .then(data => {
-                console.log(data)
+                console.log(data[data.length])
             })
             .catch(error => console.error("Error loading devotionals:", error));
